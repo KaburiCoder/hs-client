cd671e0e68f316c4e1c591cfd23bf39533f15dc7
Kaburi <specialemail90@gmail.com>
2024-05-02 15:17:25 +0900

    .

--- src/app/(account)/layout.tsx
+++ src/app/(account)/layout.tsx
@@ -1,12 +1,13 @@
 import { paths } from "@/paths";
 import { ChildrenProps } from "kbr-nextjs-shared/props";
+import Image from "next/image";
 import React from "react";
 
 export default function AccountLayout({ children }: ChildrenProps) {
   return (
     <div className="flex flex-col items-center">
       <header className="flex h-28 items-center justify-center">
-        <img className="w-[200px] h-[58px]" src={paths.images.mainLogo} alt="MainLogo" width={200} height={58}/>
+        <Image src={paths.images.mainLogo} alt="MainLogo" width={200} height={58}/>
       </header>
       {children}
     </div>
