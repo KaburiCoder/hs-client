65045277709e1e67e236aba036ee6de19d8683ed
Kaburi <specialemail90@gmail.com>
2024-05-02 15:11:37 +0900

    .

--- src/app/(account)/layout.tsx
+++ src/app/(account)/layout.tsx
@@ -7,7 +7,7 @@ export default function AccountLayout({ children }: ChildrenProps) {
   return (
     <div className="flex flex-col items-center">
       <header className="flex h-28 items-center justify-center">
-        <Image src={paths.images.mainLogo} alt="MainLogo" width={200} height={58}/>
+        <Image className="w-[200px] h-[58px]" src={paths.images.mainLogo} alt="MainLogo" width={200} height={58}/>
       </header>
       {children}
     </div>
