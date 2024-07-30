0f6d2fb91fa020fca199ddbcab5cb8f5d6ff6cbf
Kaburi <specialemail90@gmail.com>
2024-07-31 08:46:28 +0900

    소켓통신 네임스페이스 추가

--- src/contants/env-data.ts
+++ src/contants/env-data.ts
@@ -4,5 +4,6 @@ export class EnvData {
   public static NEXT_ENV: string = process.env.NEXT_ENV!;
   public static DOMAIN: string = process.env.NEXT_PUBLIC_CLIENT_DOMAIN!;
   public static CLIENT_URL: string = process.env.NEXT_PUBLIC_CLIENT_URL!;
+  public static SOCKET_URL: string = process.env.NEXT_PUBLIC_SOCKET_URL!;
   public static SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL!;
 }
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -6,15 +6,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SocketIOProvider } from "kbr-nextjs-shared";
 import { useMobileVh } from "kbr-react-hooks";
 import { EnvData } from "@/contants/env-data";
+import { usePathname } from "next/navigation";
 
 const queryClient = new QueryClient();
 export function Providers({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const namespace = getSocketNamespace(pathname);
   useMobileVh();
 
+  console.log(`${EnvData.SOCKET_URL}${namespace}`);
+  
   return (
     <NextUIProvider className="min-h-screen bg-background font-sans antialiased">
       <QueryClientProvider client={queryClient}>
-        <SocketIOProvider uri={EnvData.CLIENT_URL} path="/api/socket.io">
+        <SocketIOProvider uri={`${EnvData.SOCKET_URL}${namespace}`}>
           <WindowScrollProvider>
             {children}
             <Toaster />
@@ -23,4 +28,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
       </QueryClientProvider>
     </NextUIProvider>
   );
+}
+
+function getSocketNamespace(pathname: string) {  
+  if (pathname.startsWith("/clickdesk")) return "/click-desk";
+  return "/hs";
 }
