6ef304d0da2ef5ed328bfca01d043fe7abd1809c
Kaburi <specialemail90@gmail.com>
2024-08-11 18:09:47 +0900

    이미지 로드 방식 개선

--- src/components/(admin)/clickdesk/ad/ui/AdCard.tsx
+++ src/components/(admin)/clickdesk/ad/ui/AdCard.tsx
@@ -2,33 +2,40 @@ import { paths } from "@/paths";
 import { Card, Image as NextImage } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useAdDelete } from "../_hooks/use-ad-delete";
+import { cn } from "@/lib/utils";
 
 interface Props {
   id: string;
   fileName: string;
 }
 export const AdCard = ({ id, fileName }: Props) => {
-  const [imageSrc, setImageSrc] = useState(paths.images.loadingQute);
   const { deleteFileAndImage, isPending } = useAdDelete();
-
-  useEffect(() => {
-    let isMounted = true;
-    // 이미지 로드 시작 시
-    const img = new Image();
-    img.src = `${process.env.NEXT_PUBLIC_CLIENT_URL}/api/images/${fileName}`;
-
-    img.onload = () => {
-      if (isMounted) setImageSrc(img.src); // 이미지 로드 완료 후 실제 이미지 설정
-    };
-
-    return () => {
-      isMounted = false;
-    };
-  }, [fileName]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <Card className="relative max-h-[20rem] min-h-[5rem] max-w-[20rem] flex-grow">
-      <img alt=".." className="object-bottom" src={imageSrc} loading="lazy" />
+    <Card
+      className={cn(
+        "relative max-h-[20rem] min-h-[5rem] max-w-[20rem] flex-grow",
+        isLoaded ? "" : "min-h-[20rem] min-w-[20rem]",
+      )}
+    >
+      <div>
+        {!isLoaded && (
+          <img
+            className="object-cover "
+            alt=".."
+            src={paths.images.loadingQute}
+            loading="lazy"
+          />
+        )}
+        <img
+          className={cn("object-cover", isLoaded ? "" : "hidden")}
+          alt=".."
+          src={`${process.env.NEXT_PUBLIC_CLIENT_URL}/api/images/${fileName}`}
+          loading="lazy"
+          onLoad={() => setIsLoaded(true)}
+        />
+      </div>
       <button
         disabled={isPending}
         className="absolute right-1 top-1 z-10 text-xl"
