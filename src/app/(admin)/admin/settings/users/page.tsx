c781cf5b735007e640687b57f8f82895918e5e71
Kaburi <specialemail90@gmail.com>
2024-09-15 15:21:54 +0900

    .

--- src/app/(admin)/admin/settings/users/page.tsx
+++ src/app/(admin)/admin/settings/users/page.tsx
@@ -1,8 +1,6 @@
 import AdminUsersBody from "@/components/(admin)/users/admin-users-body";
 import React from "react";
-import { unstable_noStore as noStore } from "next/cache";
 
 export default function AdminSettingsUsersPage() {
-  noStore();
   return <AdminUsersBody />;
 }
